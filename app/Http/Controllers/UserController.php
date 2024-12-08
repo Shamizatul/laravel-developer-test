cf02c7723a6a2e3120ec62ce535b181f5c3e2c50
Shamizatul <farainshamsul97@gmail.com>
2024-12-08 23:22:44 +0800

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,70 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users, 200);
+    }
+
+    public function store(UserRequest $request)
+    {
+        Log::info('Store method hit:', $request->all());
+
+        $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+
+        return response()->json($user, 201);
+    }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        return response()->json($user, 200);
+    }
+
+    public function update(UserRequest $request, $id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        $data = $request->validated();
+        if (isset($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+
+        $user->update($data);
+
+        return response()->json($user, 200);
+    }
+
+    public function delete($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted successfully'], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users/all', [UserController::class, 'index']); // To get all users
+Route::post('/users/add', [UserController::class, 'store']); // To create a new user
+Route::get('/users/{id}', [UserController::class, 'show']); // To get a specific user
+Route::put('/users/edit/{id}', [UserController::class, 'update']); // To update a user
+Route::delete('/users/delete/{id}', [UserController::class, 'delete']); // To delete a user
